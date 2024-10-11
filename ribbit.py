cf19edcef8e88eabd5e174e1188df3d296bc6084
Charles J McDonald <cjmcdonald42@gmail.com>
2024-10-11 13:32:46 -0400

    Create ribbit.py
    Pseudocode

---
+++ ribbit.py
@@ -0,0 +1,14 @@
+# ribbit
+
+# Start by introducing the game and sharing an overview of the rules.
+
+# Get names of contestants
+# Player 1 is the Swampmonster. Get their name and their secret code.
+# Player 2 is the Amphibian-Tamer. Ask their name and their guess. They get 10 tries to break the code.
+
+# Have Player 1 score Player 2's guess.
+
+# Display the turn number, the guess, and the score
+
+# Repeat until Player 2 gets it right or has used all 10 tries.
+

c4263292c0df3f67b2e69cd3ed2d43a6d6fe5fdd
Charles J McDonald <cjmcdonald42@gmail.com>
2024-10-11 14:13:15 -0400

    Update ribbit.py

--- ribbit.py
+++ ribbit.py
@@ -1,10 +1,19 @@
 # ribbit
 
 # Start by introducing the game and sharing an overview of the rules.
+print("""
+
+Welcome to Ribbit!
+This is a code breaking challenge in which the Swampmaster, Player 1, creates a secret code and
+the Amphibian-Tamer, PLayer 2, must use their swampy powers to crack the code.
+
+""")
 
 # Get names of contestants
-# Player 1 is the Swampmonster. Get their name and their secret code.
+# Player 1 is the Swampmaster. Get their name and their secret code.
 # Player 2 is the Amphibian-Tamer. Ask their name and their guess. They get 10 tries to break the code.
+
+
 
 # Have Player 1 score Player 2's guess.
 

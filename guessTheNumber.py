583195fec1b3ae35a1e9a18f6bde8c6733a47401
Charles J McDonald <cjmcdonald42@gmail.com>
2024-10-16 08:15:23 -0400

    Create guessTheNumber.py

---
+++ guessTheNumber.py
@@ -0,0 +1,19 @@
+import random
+
+# Let's assume the code is a random 3-digit number
+code = random.randint(100, 999)
+guess = -1
+
+print("Welcome to the 3-digit code guesser!")
+print("I have selected a random 3-digit code. Try to guess it!")
+
+# Loop until the guess is correct
+while guess != code:
+    guess = int(input("Enter your guess: "))
+    
+    if guess < code:
+        print("Higher!")
+    elif guess > code:
+        print("Lower!")
+    else:
+        print("Congratulations! You guessed the code:", code)
